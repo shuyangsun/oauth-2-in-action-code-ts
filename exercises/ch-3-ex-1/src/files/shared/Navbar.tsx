7b428bf79d4163b0d0ec4ce4a57d2af8c5b7386f
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-17 12:42:23 -0400

    Better styling for nav bar on mobile.

--- exercises/ch-3-ex-1/src/files/shared/Navbar.tsx
+++ exercises/ch-3-ex-1/src/files/shared/Navbar.tsx
@@ -9,19 +9,21 @@ export const Navbar: FC<Props> = ({ name }: Props) => {
   return (
     <nav className="bg-slate-900 border-b border-slate-700">
       <div className="mx-auto px-6 md:px-16 lg:px-20 xl:px-32 py-4">
-        <div className="flex items-center justify-between text-gray-300">
-          <div className="inline-flex items-center space-x-2">
-            <span className="text-xl font-light">OAuth in Action:</span>
-            <a
-              href="/"
-              className="text-xl font-normal text-white bg-blue-900 px-3 py-1 rounded hover:bg-blue-800 transition-colors"
-            >
-              {name}
-            </a>
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between text-gray-300 space-y-3 md:space-y-0">
+          <div className="flex flex-col md:inline-flex md:flex-row md:items-center space-y-2 md:space-y-0">
+            <span className="text-xl font-light">
+              OAuth in Action:
+              <a
+                href="/"
+                className="text-xl font-normal text-white bg-blue-900 px-3 py-1 rounded hover:bg-blue-800 transition-colors self-start ml-2"
+              >
+                {name}
+              </a>
+            </span>
           </div>
-          <span>
+          <div className="md:inline">
             <ServerStatus />
-          </span>
+          </div>
         </div>
       </div>
     </nav>
--- exercises/ch-3-ex-1/src/files/shared/ServerStatus.tsx
+++ exercises/ch-3-ex-1/src/files/shared/ServerStatus.tsx
@@ -2,7 +2,7 @@ import { FC } from 'hono/jsx';
 
 export const ServerStatus: FC = () => {
   return (
-    <div className="flex items-center space-x-5">
+    <div className="flex flex-col md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-5">
       <div className="flex items-center space-x-1.5">
         <span className="text-gray-300 text-lg">Client</span>
         <span
