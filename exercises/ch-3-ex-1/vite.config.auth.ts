754ef6a9132599122463acd33f0108cf88af54ee
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-17 19:27:48 -0400

    Remove static copy of tailwind.

--- exercises/ch-3-ex-1/vite.config.auth.ts
+++ exercises/ch-3-ex-1/vite.config.auth.ts
@@ -3,23 +3,10 @@ import devServer from '@hono/vite-dev-server';
 import build from '@hono/vite-build/bun';
 // @ts-expect-error: ts config from vite is wrong
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../packages/shared/*.js',
-          dest: './src/files/shared',
-        },
-        {
-          src: '../../packages/shared/style.css',
-          dest: '.',
-        },
-      ],
-    }),
     devServer({
       entry: 'src/authorization-server.tsx',
     }),
--- exercises/ch-3-ex-1/vite.config.client-solution.ts
+++ exercises/ch-3-ex-1/vite.config.client-solution.ts
@@ -3,19 +3,10 @@ import devServer from '@hono/vite-dev-server';
 import build from '@hono/vite-build/bun';
 // @ts-expect-error: ts config from vite is wrong
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../packages/shared/*.js',
-          dest: 'files/shared',
-        },
-      ],
-    }),
     devServer({
       entry: 'src/client-solution.tsx',
     }),
