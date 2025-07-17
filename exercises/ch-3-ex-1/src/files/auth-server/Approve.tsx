c1dd3899269d919a67f474311e66f9c55422e823
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-17 12:33:12 -0400

    Approve page showing values.

--- exercises/ch-3-ex-1/src/files/auth-server/Approve.tsx
+++ exercises/ch-3-ex-1/src/files/auth-server/Approve.tsx
@@ -12,10 +12,18 @@ interface Prop {
 const Scope: FC<{ scopes: string[] }> = ({ scopes }: { scopes: string[] }) => {
   return (
     <>
-      <h2>Scope</h2>
+      <p>The client is requesting access to the following:</p>
       <ul>
         {scopes.map((scope) => (
-          <li key={scope}>scope</li>
+          <li key={scope}>
+            <input
+              type="checkbox"
+              name={`scope_${scope}`}
+              id={`scope_${scope}`}
+              checked
+            />
+            <label htmlFor={`scope_${scope}`}>{scope}</label>
+          </li>
         ))}
       </ul>
     </>
@@ -30,13 +38,30 @@ export const Approve: FC<Prop> = ({
   return (
     <Layout>
       <Navbar name="OAuth Authorization Server" />
-      <div className="max-w-4xl mx-auto px-6 py-12 space-y-8">
-        <h2>Approve this client?</h2>
-        <p>
-          ID: <span>{clientConfig.clientId}</span>
-        </p>
-        {scopes && <Scope scopes={scopes} />}
-        <p>TODO: approve page {requestId}</p>
+      <div className="container">
+        <div className="jumbotron">
+          <h2>Approve this client?</h2>
+          <p>
+            <b>ID:</b> <code>{clientConfig.clientId}</code>
+          </p>
+
+          <form className="form" action="/approve" method="post">
+            <input type="hidden" name="reqid" value={requestId} />
+            {scopes && <Scope scopes={scopes} />}
+            <input
+              type="submit"
+              className="btn btn-success"
+              name="approve"
+              value="Approve"
+            />
+            <input
+              type="submit"
+              className="btn btn-danger"
+              name="deny"
+              value="Deny"
+            />
+          </form>
+        </div>
       </div>
     </Layout>
   );
