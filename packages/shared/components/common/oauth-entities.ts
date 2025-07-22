31114498ecf1180e69742691131d15f05e62accb
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-22 09:04:32 -0400

    Add OAuth entity type.

---
+++ packages/shared/components/common/oauth-entities.ts
@@ -0,0 +1,14 @@
+export type OAuthEntity = 'client' | 'auth_server' | 'protected_resource';
+
+export function getOAuthEntityName(entity: OAuthEntity): string {
+  switch (entity) {
+    case 'client':
+      return 'OAuth Client';
+    case 'auth_server':
+      return 'OAuth Authorization Server';
+    case 'protected_resource':
+      return 'OAuth Protected Resource';
+    default:
+      throw new Error(`Unknown entity: ${entity}`);
+  }
+}
