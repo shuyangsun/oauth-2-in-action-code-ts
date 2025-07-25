b8fd507537d7303f515e3b852737971d0a721ce2
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-25 08:52:02 -0400

    Chagne page title.

--- packages/shared/components/common/Layout.tsx
+++ packages/shared/components/common/Layout.tsx
@@ -15,7 +15,7 @@ export const Layout: FC<LayoutProps> = ({ oauthEntity, children }) => {
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>OAuth in Action: OAuth Client</title>
+        <title>OAuth 2 in Action</title>
         {/* Using Vite to link Tailwind CSS with Components in shared package is not working. */}
         {/* <link href="/src/style.css" rel="stylesheet" /> */}
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
